36a6240504b68949c0d3c2910bd3349577d4a626
Pyeon0904 <jjp2548@gmail.com>
2025-01-23 09:57:34 +0900

    feat: CommentsQuery

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,14 +1,33 @@
-import { fetchComments } from "./api";
 import "./PostDetail.css";
+import {useQuery} from "@tanstack/react-query";
+import {fetchComments} from "./api.js";
 
-export function PostDetail({ post }) {
+export function PostDetail({post}) {
   // replace with useQuery
-  const data = [];
+  // const data = []; // 초기값
+  const {data, isError, error, isLoading} = useQuery({
+    queryKey: ["comments", post.id],
+    queryFn: () => fetchComments(post.id),
+    staleTime: 2000, // 2s
+  });
+
+  if (isLoading) {
+    return <h4>Loading..</h4>;
+  }
+  if (isError) {
+    return (
+      <>
+        <h4>Something went wrong!</h4>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <h3 style={{color: "blue"}}>{post.title}</h3>
+      <button>Delete</button>
+      <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
