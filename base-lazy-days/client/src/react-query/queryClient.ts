1a96c91c2a6679a88185db7000dedcee169c7cd2
jjpyeon <jjpyeon@org-i.com>
2025-03-19 16:57:33 +0900

    feat: onError 제공
    - queryClient.ts에서 errorHandler 작성
    ㄴ queryClient.ts에서 작성했었던 queryClient 내부에 queryCache > onError > errorHandler 작성
    ㄴ error Handler를 작성 후, 이를 queryClient에 추가하는 데에 많은 key입력이 필요치는 않다
    
    1. queryClient에 쿼리캐시 옵션 제공
    2. 쿼리캐시에는 onError 콜백이 있다.
    3. onError 콜백이 호출되면, 쿼리에서 발생한 에러가 여기 error에 전달됨
    4. 그 때, 실행될 error handler 작성

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,21 +1,32 @@
-import {QueryClient} from "@tanstack/react-query";
-// import { toast } from "@/components/app/toast";
+import {QueryCache, QueryClient} from "@tanstack/react-query";
+
+import {toast} from "@/components/app/toast";
 
 
-// function errorHandler(errorMsg: string) {
-//   // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
-//   // one message per page load, not one message per query
-//   // the user doesn't care that there were three failed queries on the staff page
-//   //    (staff, treatments, user)
-//   const id = "react-query-toast";
+function errorHandler(errorMsg: string) {
+  // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
+  // one message per page load, not one message per query
+  // the user doesn't care that there were three failed queries on the staff page
+  //    (staff, treatments, user)
+  const id = "react-query-toast";
 
-//   if (!toast.isActive(id)) {
-//     const action = "fetch";
-//     const title = `could not ${action} data: ${
-//       errorMsg ?? "error connecting to server"
-//     }`;
-//     toast({ id, title, status: "error", variant: "subtle", isClosable: true });
-//   }
-// }
+  if (!toast.isActive(id)) {
+    const action = "fetch";
+    const title = `could not ${action} data: ${
+      errorMsg ?? "error connecting to server"
+    }`;
+    toast({id, title, status: "error", variant: "subtle", isClosable: true});
+  }
+}
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  // error Handler를 작성 후, 이를 queryClient에 추가하는 데에 많은 key입력이 필요치는 않다.
+  // 1. queryClient에 쿼리캐시 옵션 제공
+  queryCache: new QueryCache({
+    // 2. 쿼리캐시에는 onError 콜백이 있다.
+    onError: (error) => { // 3. onError 콜백이 호출되면, 쿼리에서 발생한 에러가 여기 error에 전달됨
+      // 4. 그 때, 실행될 error handler 작성
+      errorHandler(error.message); // error 작성: 위에서 작성한 errorHandler는 string을 사용하기에 문자열(error.messge)을 전달해야함
+    },
+  }),
+});
