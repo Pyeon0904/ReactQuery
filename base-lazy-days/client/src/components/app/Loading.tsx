29b00cce14ccd3f94bbac02543bb33b9af45b660
jjpyeon <jjpyeon@org-i.com>
2025-03-19 15:18:21 +0900

    feat: useIsFetching 사용
    - App.tsx에서 사용하고 있는 <Loading/> 수정
    - const isFetching = false; 에 false => useIsFetching 사용
    - useIsFetching은 현재 '가져오기(fetch)상태'인 Query 호출의 수를 나타내는 number(정수)를 반환

--- base-lazy-days/client/src/components/app/Loading.tsx
+++ base-lazy-days/client/src/components/app/Loading.tsx
@@ -1,9 +1,12 @@
-import { Spinner, Text } from "@chakra-ui/react";
+import {Spinner, Text} from "@chakra-ui/react";
+import {useIsFetching} from "@tanstack/react-query";
 
 export function Loading() {
-  // will use React Query `useIsFetching` to determine whether or not to display
-  const isFetching = false; // for now, just don't display
+  const isFetching = useIsFetching(); // useIsFetching이 반환하는 무엇이든으로 바꿀 수 있다.
+  // 위 useIsFetching은 현재 '가져오기(fetch)상태'인 Query 호출의 수를 나타내는 number(정수)를 반환한다.
   const display = isFetching ? "inherit" : "none";
+  // 따라서 isFetching이 0보다 크면 Fetching 상태의 호출이 있고, '참'이기에 inherit = Loading Spinner 표시!
+  // 현재 가져오는 항목이 0인 경우, 0은 false이기에 none! = Loading Spinner 표시X
 
   return (
     <Spinner
