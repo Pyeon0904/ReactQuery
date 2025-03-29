6de8d531ccc25af6f46c99e78b92d91ce0c1b19d
Pyeon0904 <jjp2548@gmail.com>
2025-03-29 16:27:13 +0900

    feat: prefetching 사용 - useTreatment
    - useTreatment hooks에 코드기입
    - prefetch 사용을 위해 hook 내 queryClient 호출
    - queryClient에서 제공하는 queryClient.prefetchQuery() 사용
    - queryKey와 queryFn은 동일한 것을 사용!
    - prefetch 타겟 컴포넌트에 prefetch() 기입!

--- base-lazy-days/client/src/components/app/Home.tsx
+++ base-lazy-days/client/src/components/app/Home.tsx
@@ -1,14 +1,16 @@
-import { Icon, Stack, Text } from "@chakra-ui/react";
-import { GiFlowerPot } from "react-icons/gi";
+import {Icon, Stack, Text} from "@chakra-ui/react";
+import {GiFlowerPot} from "react-icons/gi";
 
-import { BackgroundImage } from "@/components/common/BackgroundImage";
+import {BackgroundImage} from "@/components/common/BackgroundImage";
+import {usePrefetchTreatments} from "@/components/treatments/hooks/useTreatments";
 
 export function Home() {
+  usePrefetchTreatments(); // prefetch 전용 hooks 사용
   return (
     <Stack textAlign="center" justify="center" height="84vh">
-      <BackgroundImage />
+      <BackgroundImage/>
       <Text textAlign="center" fontFamily="Forum, sans-serif" fontSize="6em">
-        <Icon m={4} verticalAlign="top" as={GiFlowerPot} />
+        <Icon m={4} verticalAlign="top" as={GiFlowerPot}/>
         Lazy Days Spa
       </Text>
       <Text>Hours: limited</Text>
