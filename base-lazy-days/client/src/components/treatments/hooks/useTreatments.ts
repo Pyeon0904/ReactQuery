b4d37595b4d37109e665353440d6aac741da8161
JunJang Pyeon <63281505+Pyeon0904@users.noreply.github.com>
2025-03-29 16:29:41 +0900

    feat: prefetching 사용 - useTreatment (#18)
    - useTreatment hooks에 코드기입
    - prefetch 사용을 위해 hook 내 queryClient 호출
    - queryClient에서 제공하는 queryClient.prefetchQuery() 사용
    - queryKey와 queryFn은 동일한 것을 사용!
    - prefetch 타겟 컴포넌트에 prefetch() 기입!

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,4 +1,4 @@
-import {useQuery} from "@tanstack/react-query";
+import {useQuery, useQueryClient} from "@tanstack/react-query";
 
 import type {Treatment} from "@shared/types";
 
@@ -18,6 +18,13 @@ export function useTreatments(): Treatment[] {
     queryKey: [queryKeys.treatments],
     queryFn: getTreatments,
   });
-
   return data;
+}
+
+export function usePrefetchTreatments(): void { // 아무것도 반환하지 않는 이유는, cache를 채우는 것이기에
+  const queryClient = useQueryClient(); // prefetchQuery를 가져오기 위해 useQueryClient 호출
+  queryClient.prefetchQuery({
+    queryKey: [queryKeys.treatments], // QueryKey는 동일한 것으로!
+    queryFn: getTreatments, // QueryFn()도 동일한 것으로!
+  });
 }
