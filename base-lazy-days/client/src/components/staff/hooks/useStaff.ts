349156e4f064629f3dfa6bb655159c3e82320df1
Pyeon0904 <jjp2548@gmail.com>
2025-04-09 20:41:45 +0900

    feat: selectFn()_useStaff
    - useCallback 사용해서 selectFn을 만들어주기
    - useQuery 내에 queryKey, queryFn, select 추가로 작성해주기

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,26 +1,49 @@
 import {useQuery} from "@tanstack/react-query";
-import {useState} from "react";
+import {useCallback, useState} from "react";
 
 import type {Staff} from "@shared/types";
+
+import {filterByTreatment} from "../utils";
 
 import {axiosInstance} from "@/axiosInstance";
 import {queryKeys} from "@/react-query/constants";
 
-// query function for useQuery
 async function getStaff(): Promise<Staff[]> {
-  const {data} = await axiosInstance.get('/staff');
+  const {data} = await axiosInstance.get("/staff");
   return data;
 }
+
+// alternative to defining inside the hook with `useCallback`,
+//   in lines 31 - 37
+// suggested by Niv Bekelman in this Q&A thread
+//   https://www.udemy.com/course/learn-react-query/learn/#questions/21529264/
+// const selectFn = (unfilteredStaff: Staff[], filter: string) => {
+//   if (filter === "all") return unfilteredStaff;
+//   return filterByTreatment(unfilteredStaff, filter);
+// };
 
 export function useStaff() {
   // for filtering staff by treatment
   const [filter, setFilter] = useState("all");
 
+  const selectFn = useCallback(
+    (unfilteredStaff: Staff[]) => {
+      if (filter === "all") return unfilteredStaff; // fiter의 값이 "all" => unfilteredStaff(필터링할 항목이 없는 상태) 반환
+      return filterByTreatment(unfilteredStaff, filter);
+    }, [filter]); // 아니라면, unfilteredStaff 및 filter 상태값이 무엇이든, utils에 작성되어 있는 filterByTreatment 함수를 실행해 treatment별로 필터링된 직원이 반환
+  // filter 라는 종속성을 둔다!
+  // filter가 변경될 때마다 이 선택기가 다시 실행되도록 하려면,
+  // filter가 변경되면 데이터가 변경되지 않더라도 선택 함수가 변경된 다음 선택기가 다시 실행되도록 해야한다.
+
   const fallback: Staff[] = [];
-  // const staff: Staff[] = [];
   const {data: staff = fallback} = useQuery({
     queryKey: [queryKeys.staff],
     queryFn: getStaff,
+    select: selectFn,
+    // or, if the selectFn is defined outside the hook
+    //   as show in lines 20 - 23
+    // select: (data) => selectFn(data, filter)
   });
+
   return {staff, filter, setFilter};
 }
